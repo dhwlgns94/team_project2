355392841ff9cb7d4e972b0197679ead60285b34
dhwlgns94 <153040883+dhwlgns94@users.noreply.github.com>
2024-01-16 21:40:47 +0900

    Add files via upload

--- suvpage/6_product_wood/js/main.js
+++ suvpage/6_product_wood/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     // 문서준비이벤트시작
+$(".sec_3 .inner .list").addClass("off");
 
+
+$('.sti > li:nth-child(1)').click(function(){
+    $("#sec_3 .inner .list:nth-child(1)").removeClass("off").siblings().addClass("off");
+});
+$('.sti > li:nth-child(2)').click(function(){
+    $("#sec_3 .inner .list:nth-child(2)").removeClass("off").siblings().addClass("off");
+});
+$('.sti > li:nth-child(3)').click(function(){
+    $("#sec_3 .inner .list:nth-child(3)").removeClass("off").siblings().addClass("off");
+});
 
 
     
