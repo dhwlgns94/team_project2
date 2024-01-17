253f77a5999dc6e1b6ca0c1d9bb8e88950fcdb94
dhwlgns94 <153040883+dhwlgns94@users.noreply.github.com>
2024-01-17 19:53:50 +0900

    Add files via upload

--- suvpage/6_product_wood/js/main.js
+++ suvpage/6_product_wood/js/main.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
     // 문서준비이벤트시작
-$(".sec_3 .inner .list").addClass("off");
-
-
-$('.sti > li:nth-child(1)').click(function(){
-    $("#sec_3 .inner .list:nth-child(1)").removeClass("off").siblings().addClass("off");
-});
-$('.sti > li:nth-child(2)').click(function(){
-    $("#sec_3 .inner .list:nth-child(2)").removeClass("off").siblings().addClass("off");
-});
-$('.sti > li:nth-child(3)').click(function(){
-    $("#sec_3 .inner .list:nth-child(3)").removeClass("off").siblings().addClass("off");
-});
+    $('.product').hide();
+    $('.product:nth-child(1)').show();
+    $('.tab_btn').click(function(){
+        $(this).addClass('on').siblings().removeClass('on');
+        let Num = $(this).index();
+        $(".prd>.product").eq(Num).siblings().hide();
+        $(".prd>.product").eq(Num).fadeIn();
+    });
 
 
     
