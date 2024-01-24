7afbcf05036fd3188ae0512eb6aea80c00f7c631
dhwlgns94 <153040883+dhwlgns94@users.noreply.github.com>
2024-01-24 17:58:07 +0900

    Add files via upload

---
+++ directory_bank/g_head.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="Title" content="교하산업" />
+    <meta name="description" content="철강산업의 출발점 교하산업">
+    <meta name="robots" content="all" />
+    <meta http-equiv="Content-Script-Type" content="html/css/js" />
+    <meta http-equiv="Subject" content="철강 도급 산업 파렛트 각목 목재박스 지관 코일 외주링 내주링 측면단판" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Author" content="it507_team_project2" />
+    <meta http-equiv="Other Agent" content="프로젝트팀장 오지훈" />
+    <meta http-equiv="Generator" content="Visual_Studio_Code figma photoshop Illustrator" />
+    <meta http-equiv="Reply-To" content="dhwlgns94@gmail.com" />
+    <meta http-equiv="Email" content ="dhwlgns94@gmail.com" />
+    <meta http-equiv="Build" content="2024-01-20" />
+    <meta http-equiv="imagetoolbar" content="no" />
+    <!-- 그림 위 마우스 오버 시 이미지 관련 툴바 생기지 않게 하기 -->
+
+    <meta http-equiv="Cache-Control" content="no-cache" />
+    <meta http-equiv="Pragma" content="no-chche" />
+    <!-- 캐쉬저장금지코드 항상 바뀐 내용을 보여줄 수 있음(페이지를 cache에서 가져오지 않게 하여 시간은 많이걸림) -->
+
+    <meta http-equiv="Page-Enter" content="RevealTrans(Duration=10, Transition=50)">, <meta http-equiv="Page-Exit" content="RevealTrans(Duration=10, Transition=50)">
+    <!-- 페이지 전환 효과 -->
+
+
+    <title>교하산업</title>
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://dhwlgns94.github.io/team_project2/">
+    <meta property="og:title" content="교하산업">
+    <meta property="og:site_name" content="교하산업">
+    <meta property="og:description" content="철강산업의 출발점 교하산업">
+    <meta property="og:image" content="img/og_img/og_image.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:locale" content="korea">
+
+    <link rel="icon" href="../img/pavicon/favicon.ico">
+
+    <!-- 풀페이지 링크-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/vendors/easings.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.js"></script>
+
+    <link rel="stylesheet" href="../css/font.css">
+    <!-- 폰트 디렉토리에다가 ctrl+클릭해서 확인 가능 -->
+    <!-- font폴더 내의 font.html에서 전체적인 폰트 확인 가능 -->
+    <link rel="stylesheet" href="../css/reset.css">
+    <!-- reset body #wrap .inner 등등 -->
+
+
+    <link rel="stylesheet" href="../css/common.css">
+    <!-- header footer 이외에 코딩금지 -->
+    <!-- 해더 푸터는 상단고정이므로 양식으로 포함-->
+    <link rel="stylesheet" href="../css/main.css">
+    <!-- 해당페이지의 #main 이외에 코딩금지 -->
+
+    <link rel="stylesheet" href="../css/reactive.css">
+    <!-- 해당페이지의 반응형 -->
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- 제이쿼리 371 -->
+
+
+
+
+    <script src="../js/default.js"></script>
+    <!-- header footer 이외에 코딩금지 -->
+    <!-- 해더푸터는 상단고정이므로 양식으로 포함-->
+
+    <script src="../js/main.js"></script>
+    <!-- 헤당페이지의 #main 이외에 코딩금지 -->
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/vendors/easings.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.js"></script>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollTrigger.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.1/ScrollToPlugin.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <!-- 스와이퍼 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
+    <!-- 스와이퍼 -->
+
+</head>
+
+<body>
+    <div class="sitemap">
+
+        <div class="sitemap_left"></div>
+        <div class="sitemap_right">
+
+                <ul class="sitemap_gnb">
+                    <li class="kr_p2"><a href="../suvpage/1_introduce/index.html">교하산업</a></li>
+                    <li class="kr_p2"><a href="../suvpage/2_place_Gangdong/index.html">사업장안내</a>
+                        <ul class="sitemap_depth2">
+                            <li class="kr_p3"><a href="../suvpage/2_place_Gangdong/index.html"><span>강동공장 (본사)</span></a>
+                            </li>
+                            <li class="kr_p3"><a href="../suvpage/3_place_Singi/index.html"> <span>신기동공장</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="kr_p2"><a href="../suvpage/4_product_ctl/index.html">제품안내</a>
+                        <ul class="sitemap_depth2">
+                            <li class="kr_p3"><a href="../suvpage/4_product_ctl/index.html"><span>CTL 코일</span></a></li>
+                            <li class="kr_p3"><a
+                                    href="../suvpage/5_product_coil_packing/index.html"><span>내주링·외주링</span></a>
+                            </li>
+                            <li class="kr_p3"><a href="../suvpage/6_product_wood/index.html"><span>LVL
+                                        파렛트·각목·목재박스</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="kr_p2"> <a href="../suvpage/8_cs_connection/index.html">문의하기</a>
+                        <ul class="sitemap_depth2">
+                            <li class="kr_p3"><a href="#"><span>공지사항</span></a></li>
+                            <li class="kr_p3"><a href="../suvpage/8_cs_connection/index.html"><span>협업 및 고객문의</span></a>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+
+
+        </div>
+    </div>
+    <div id="wrap">
+        <div class="depth2_bg">
+            <div class="depth2_bg_inner">
+
+                <div class="depth2_con gnb_1">
+
+
+                    <ul class="gnb_1_right">
+                        <li class="kr_p2">교하산업은 </li>
+                        <li class="kr_p3">철강산업 발전에 이바지하며,<br>
+                            고품질의 상품을 제공하기 위해 지속적으로 노력하고 있습니다.</li>
+                    </ul>
+
+                    <div class="gnb_1_left">
+                        <img src="../img/header/depth2/depth2_1.png" alt="#">
+                    </div>
+                </div>
+
+
+
+
+
+                <div class="depth2_con gnb_2">
+
+
+                    <ul class="gnb_1_right">
+                        <li class="kr_p2"><span class="kr_p3">경북 경주시 강동면 강동산단로 68 </span>강동공장(본사)</li>
+
+                        <li class="kr_p2 gnb_2_margin"><span class="kr_p3">경북 포항시 남구 대송면 제내길31번길 17</span>신기동공장</li>
+
+                    </ul>
+                    <div class="gnb_1_left">
+                        <img src="../img/header/depth2/depth2_2.png" alt="#">
+                    </div>
+                </div>
+
+                <div class="depth2_con gnb_3">
+
+                    <ul class="gnb_1_right">
+                        <li class="kr_p2">공업용 포장자재 뿐만 아니라</li>
+                        <li class="kr_p2">변압기 코아 또한 생산력을 향상하고 있습니다.</li>
+                    </ul>
+
+
+                    <div class="gnb_1_left">
+                        <img src="../img/header/depth2/depth2_3.png" alt="#">
+                    </div>
+
+                </div>
+
+                <div class="depth2_con gnb_4">
+
+                    <ul class="gnb_1_right">
+                        <li class="kr_p2">서로 협력하여 다양한 성과를 이루어보세요.</li>
+                        <li class="kr_p2">어떤 문의든 환영합니다!</li>
+                    </ul>
+
+
+                    <div class="gnb_1_left">
+                        <img src="../img/header/depth2/depth2_4.png" alt="#">
+                    </div>
+
+
+                </div>
+
+            </div>
+            <!-- inner -->
+
+        </div>
+        <header id="header">
+            <div class="inner">
+                <div class="logo">
+                    <a href="../index.html"><img src="../img/header/logo_header.svg" alt="#"></a>
+                </div>
+
+                <ul class="gnb">
+                    <li class="kr_p2"><a href="../suvpage/1_introduce/index.html">교하산업</a></li>
+                    <li class="kr_p2"><a href="../suvpage/2_place_Gangdong/index.html">사업장안내</a>
+                        <ul class="depth2">
+                            <li class="kr_p3"><a href="../suvpage/2_place_Gangdong/index.html"><span>강동공장 (본사)</span></a>
+                            </li>
+                            <li class="kr_p3"><a href="../suvpage/3_place_Singi/index.html"> <span>신기동공장</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="kr_p2"><a href="../suvpage/4_product_ctl/index.html">제품안내</a>
+                        <ul class="depth2">
+                            <li class="kr_p3"><a href="../suvpage/4_product_ctl/index.html"><span>CTL 코일</span></a></li>
+                            <li class="kr_p3"><a
+                                    href="../suvpage/5_product_coil_packing/index.html"><span>내주링·외주링</span></a>
+                            </li>
+                            <li class="kr_p3"><a href="../suvpage/6_product_wood/index.html"><span>LVL
+                                        파렛트·각목·목재박스</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="kr_p2"> <a href="../suvpage/8_cs_connection/index.html">문의하기</a>
+                        <ul class="depth2">
+                            <li class="kr_p3"><a href="#"><span>공지사항</span></a></li>
+                            <li class="kr_p3"><a href="../suvpage/8_cs_connection/index.html"><span>협업 및 고객문의</span></a>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+
+                <div class="btn_ham_box">
+                    <ul class="btn_ham">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- inner -->
+        </header>
+        <!-- header -->
+
+
+        <main id="main">
